c16fec7e655f493447e8d47a6cf465ab7584bcbe
40327382 <ggoodwin04@qub.ac.uk>
2024-02-07 18:04:50 +0000

    Transaction Class

--- src/Model/User.java
+++ src/Model/User.java
@@ -7,6 +7,15 @@ public class User {
 	private String last_name;
 	private String mobile_number;
 	
+	public User(String username, String password, String first_name, String last_name, String mobile_number) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.mobile_number = mobile_number;
+	}
+	
 	public String getUsername() {
 		return username;
 	}
@@ -48,14 +57,7 @@ public class User {
 		this.mobile_number = mobile_number;
 	}
 
-	public User(String username, String password, String first_name, String last_name, String mobile_number) {
-		super();
-		this.username = username;
-		this.password = password;
-		this.first_name = first_name;
-		this.last_name = last_name;
-		this.mobile_number = mobile_number;
-	}
+	
 
 }
 
