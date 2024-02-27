c4f89e214e7ba0c92b0d36199e26bd75dc79e264
40327382 <ggoodwin04@qub.ac.uk>
2024-02-27 16:56:14 +0000

    Added DataUtilities test

--- Lab2/src/org/jfree/data/test/RangeTest.java
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -16,14 +16,14 @@ public class RangeTest {
 		rangeObjectUnderTest = new Range(-1,1);
 	}
 	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
 	@Test
 	public void testCentralValueShouldBeZero() {
 		assertEquals("The central value of -1 and 1 should be 0",
 					0, rangeObjectUnderTest.getCentralValue(), 0.000000001d);
-	}
-	
-	@After
-	public void tearDown() throws Exception {
 	}
 
 }
