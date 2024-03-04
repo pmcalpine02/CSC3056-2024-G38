5179bf1963d2bf01d4804316f65c6bbc4e61238e
40327382 <ggoodwin04@qub.ac.uk>
2024-03-04 14:59:18 +0000

    expand tests

--- Lab2/src/org/jfree/data/test/RangeTest.java
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -2,6 +2,8 @@ package org.jfree.data.test;
 
 import static org.junit.Assert.*;
 
+import java.security.InvalidParameterException;
+
 import org.jfree.data.Range;
 import org.junit.After;
 import org.junit.Before;
@@ -14,22 +16,24 @@ public class RangeTest {
 	private Range positiveRange;
 	private Range negAndPosRange;
 	private Range combinedRange;
+	private Range expandedRange;
 	
 	@Before
 	public void setUp() throws Exception {
 		rangeObjectUnderTest = new Range(-1,1);
 		negativeRange = new Range(-10,-1);
 		positiveRange = new Range(20,31);
-		negAndPosRange = new Range(-20,25);
-		
-		System.out.println(negAndPosRange.toString());
+		negAndPosRange = new Range(-20,25);		
 	}
 	
 	@After
 	public void tearDown() throws Exception {
+		rangeObjectUnderTest = null;
 		negativeRange = null;
 		positiveRange = null;
+		negAndPosRange = null;
 		combinedRange = null;
+		expandedRange = null;
 	}
 	
 	// Combine method tests
@@ -109,4 +113,105 @@ public class RangeTest {
 		assertEquals("The central value of -1 and 1 should be 0",
 					0, rangeObjectUnderTest.getCentralValue(), 0.000000001d);
 	}
+	
+	// expand tests
+	@Test
+	public void testExpandbyZeroOnBothMargins() {
+		//exercise
+		expandedRange = Range.expand(negAndPosRange, 0, 0);
+		//verify
+		assertEquals("The range should be unchanged",
+				negAndPosRange, expandedRange);
+	}
+	
+	@Test
+	public void testExpandLowerAndUpperMarginsBetweenZeroAndOne() {
+		//exercise
+		
+		expandedRange = Range.expand(negAndPosRange, 0.25, 0.75);
+		//verify
+		assertEquals("Range expanded by 25% and 75% on both sides respectively",new Range(-31.25,58.75),expandedRange);
+	}
+	
+	@Test
+	public void testExpandLowerAndUpperMarginsBothEqualOne() {
+		//exercise
+		expandedRange = Range.expand(negAndPosRange, 1, 1);
+		//verify
+		assertEquals("Range expanded by 100% on both sides ",new Range(-65,70),expandedRange);
+	}
+
+	@Test
+	public void testExpandByLowerMarginZeroAndUpperMarginBetweenZeroAndOne() {
+		//exercise
+		expandedRange = Range.expand(negAndPosRange, 0, 0.5);
+		//verify
+		assertEquals("Upper bound of range should increase by 50%",new Range(-20,47.5),expandedRange);	
+	}
+
+	@Test
+	public void testExpandByLowerMarginBetweenZeroAndOneAndUpperMarginZero() {
+		//exercise
+		expandedRange = Range.expand(negAndPosRange, 0.5, 0);
+		//verify
+		assertEquals("Lower bound of range should increase by 50% ",new Range(-42.5,25),expandedRange);	
+	}
+
+	@Test
+	public void testExpandBothMarginsNegative() {
+		
+		try {
+			//exercise
+			expandedRange = Range.expand(negAndPosRange, -0.25, -0.25);
+			assertEquals("Range should have reduced by 25% on both sides",new Range(-8.75,13.75),expandedRange);
+		}
+		catch (Exception e) {
+			fail("Exception should not be thrown and range should have reduced by 25% on both sides");
+		}
+		
+		//verify
+		assertEquals("Range should have reduced by 25% on both sides",new Range(-8.75,13.75),expandedRange);
+	}
+
+	@Test
+	public void testExpandByLowerMarginNegativeAndUpperMarginBetweenZeroAndOne() {
+		//exercise
+		expandedRange = Range.expand(negAndPosRange, -0.25, 0.5);
+		//verify
+		System.out.println(expandedRange);
+		assertEquals("Range should have reduced by 25% on lower margin and increased by 50% on upper margin sides",new Range(-8.75,47.5),expandedRange);
+	}
+
+	@Test
+	public void testExpandByLowerMarginBetweenZeroAndOneAndUpperMarginNegative() {
+		//exercise
+		expandedRange = Range.expand(negAndPosRange, 0.5, -0.25);
+		//verify
+		assertEquals("Range should be expanded by 50% in the lower margin and then reduced by 25% in the upper",new Range(-42.5,13.75),expandedRange);
+	}
+
+	@Test
+	public void testExpandByBothMarginsGreaterThanOne() {
+		//exercise
+		expandedRange = Range.expand(negAndPosRange, 10, 10);
+		//verify
+		assertEquals("Range should expanded by 1000% on both sides",new Range(-470,475),expandedRange);
+		
+	}
+	
+	@Test
+	public void testExpandNullRange() {
+		try {
+			//exercise
+			expandedRange = Range.expand(null, 0.25, 0.75);
+			fail("No error thrown");
+		}catch(Exception e) {
+			//verify
+			System.out.println(e.getClass());
+			assertTrue("Wrong exception thrown, should be InvalidParameterException", e.getClass().equals(InvalidParameterException.class));	
+		}
+	}
+	
+	
+	
 }
